07e4bc6e36e24e9f55b5aab0251e408baecb9162
Anton Maminov <anton.linux@gmail.com>
2025-11-11 14:21:05 +0200

    Update geometric_spec.cr

--- spec/extensions/geometric_spec.cr
+++ spec/extensions/geometric_spec.cr
@@ -3,39 +3,35 @@ require "../spec_helper"
 describe "Lustra Geometric Extensions" do
   describe "Expression Engine Geometric Operations" do
     it "should generate correct distance queries" do
-      point1 = PG::Geo::Point.new(0.0, 0.0)
-      point2 = PG::Geo::Point.new(3.0, 4.0)
+      point = PG::Geo::Point.new(3.0, 4.0)
 
       # Test distance_from method
-      expression = Lustra::Expression.where { coordinates.distance_from(point2) }
-      expression.resolve.should contain("<->")
+      expression = Lustra::Expression.where { coordinates.distance_from(point) }
+      expression.resolve.should eq("(\"coordinates\" <-> point(3.0,4.0))")
     end
 
     it "should generate correct containment queries" do
       point = PG::Geo::Point.new(1.0, 1.0)
-      circle = PG::Geo::Circle.new(0.0, 0.0, 5.0)
 
       # Test contains? method
       expression = Lustra::Expression.where { search_area.contains?(point) }
-      expression.resolve.should contain("@>")
+      expression.resolve.should eq("(\"search_area\" @> point(1.0,1.0))")
     end
 
     it "should generate correct overlap queries" do
-      circle1 = PG::Geo::Circle.new(0.0, 0.0, 5.0)
-      circle2 = PG::Geo::Circle.new(3.0, 4.0, 3.0)
+      circle = PG::Geo::Circle.new(3.0, 4.0, 3.0)
 
       # Test overlaps? method
-      expression = Lustra::Expression.where { area1.overlaps?(circle2) }
-      expression.resolve.should contain("&&")
+      expression = Lustra::Expression.where { area.overlaps?(circle) }
+      expression.resolve.should eq("(\"area\" && circle(point(3.0,4.0),3.0))")
     end
 
     it "should generate correct positioning queries" do
-      point1 = PG::Geo::Point.new(0.0, 0.0)
-      point2 = PG::Geo::Point.new(5.0, 0.0)
+      point = PG::Geo::Point.new(5.0, 0.0)
 
       # Test left_of? method
-      expression = Lustra::Expression.where { coordinates.left_of?(point2) }
-      expression.resolve.should contain("<<")
+      expression = Lustra::Expression.where { coordinates.left_of?(point) }
+      expression.resolve.should eq("(\"coordinates\" << point(5.0,0.0))")
     end
 
     it "should combine distance with comparison operators" do
@@ -44,16 +40,7 @@ describe "Lustra Geometric Extensions" do
 
       # Test within_distance? method
       expression = Lustra::Expression.where { coordinates.within_distance?(point, max_distance) }
-      expression.resolve.should contain("<->")
-      expression.resolve.should contain("<=")
-    end
-  end
-
-  describe "Geometric Column Helpers" do
-    it "should define geometric column types correctly" do
-      # Test that the geometric column macros are available
-      # Since they are macros, we test that they can be used in a model context
-      true.should be_true # The fact that the library compiles means the macros work
+      expression.resolve.should eq("((\"coordinates\" <-> point(0.0,0.0)) <= 1000.0)")
     end
   end
 
