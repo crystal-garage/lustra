38b12347f041402500c5628b15aedfba068edc75
Anton Maminov <anton.linux@gmail.com>
2025-11-11 13:46:22 +0200

    remove spatial index helpers

--- src/lustra/extensions/geometric/migration.cr
+++ src/lustra/extensions/geometric/migration.cr
@@ -1,16 +1,4 @@
 module Lustra::Migration::GeometricHelpers
-  # Create spatial index for geometric columns
-  def add_gist_index(table : String, column : String, name : String? = nil)
-    index_name = name || "#{table}_#{column}_gist_idx"
-    up_sql("CREATE INDEX #{index_name} ON #{table} USING GIST (#{column})")
-  end
-
-  # Create spatial index with custom operator class
-  def add_gist_index(table : String, column : String, operator_class : String, name : String? = nil)
-    index_name = name || "#{table}_#{column}_#{operator_class}_idx"
-    up_sql("CREATE INDEX #{index_name} ON #{table} USING GIST (#{column} #{operator_class})")
-  end
-
   # Add exclusion constraint for geometric types
   def add_exclusion_constraint(table : String, column : String, operator : String = "&&", name : String? = nil)
     constraint_name = name || "#{table}_#{column}_exclusion"
@@ -26,10 +14,5 @@ module Lustra::Migration::GeometricHelpers
   # Drop geometric constraint
   def drop_geometric_constraint(table : String, constraint_name : String)
     down_sql("ALTER TABLE #{table} DROP CONSTRAINT IF EXISTS #{constraint_name}")
-  end
-
-  # Drop spatial index
-  def drop_gist_index(name : String)
-    down_sql("DROP INDEX IF EXISTS #{name}")
   end
 end
