5b8b7c26c814e92cbb456fd9d72d6850b33b5109
Anton Maminov <anton.linux@gmail.com>
2025-11-15 16:20:00 +0200

    wip

--- spec/model/range_spec.cr
+++ spec/model/range_spec.cr
@@ -19,5 +19,22 @@ module RangeSpec
         range_record.time_range.should eq(Time.utc(2024, 1, 1)..Time.utc(2024, 12, 31))
       end
     end
+
+    it "creates and retrieves records with endless/beginless range" do
+      temporary do
+        reinit_example_models
+
+        range_record = RangeData.create!(
+          name: "Test Range Record",
+          int32_range: ..10,
+          int64_range: 1000..,
+          time_range: Time.utc(2024, 1, 1)..
+        )
+
+        range_record.int32_range.should eq(...11)
+        range_record.int64_range.should eq(1000...)
+        range_record.time_range.should eq(Time.utc(2024, 1, 1)...)
+      end
+    end
   end
 end
