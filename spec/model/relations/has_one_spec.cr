9c4bedf2f524cd93c5163d0b24bd2fbdda7eb75e
Anton Maminov <anton.linux@gmail.com>
2026-01-10 17:43:00 +0200

    Update has_one_spec.cr

--- spec/model/relations/has_one_spec.cr
+++ spec/model/relations/has_one_spec.cr
@@ -14,6 +14,34 @@ describe "Lustra::Model::Relations::HasOne" do
         end
       end
 
+      it "deletes user_info" do
+        temporary do
+          reinit_example_models
+
+          user = User.create!({first_name: "John", last_name: "Doe"})
+          UserInfo.create!({registration_number: 101, user_id: user.id})
+
+          user.info.should_not be_nil
+
+          user.info!.update(bio: "Bio updated")
+          user.info!.bio.should eq("Bio updated")
+        end
+      end
+
+      it "deletes user_info" do
+        temporary do
+          reinit_example_models
+
+          user = User.create!({first_name: "John", last_name: "Doe"})
+          UserInfo.create!({bio: "Bio1", registration_number: 101, user_id: user.id})
+
+          user.info.should_not be_nil
+
+          user.info!.delete
+          user.info.should be_nil
+        end
+      end
+
       it "can eager load user_info" do
         temporary do
           reinit_example_models
