39cc12fcc73659885f182ec0226e67cfde554666
Anton Maminov <anton.linux@gmail.com>
2026-01-14 19:16:19 +0200

    fix ameba warnings

--- spec/model/introspection_spec.cr
+++ spec/model/introspection_spec.cr
@@ -12,14 +12,14 @@ module IntrospectionSpec
       temporary do
         Lustra::SQL.execute <<-SQL
           CREATE TABLE admins (
-            id BIGSERIAL PRIMARY KEY,
-            provider TEXT NOT NULL,
-            uid TEXT NOT NULL,
-            email TEXT NOT NULL,
-            raw_json JSONB,
-            created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL
+          id BIGSERIAL PRIMARY KEY,
+          provider TEXT NOT NULL,
+          uid TEXT NOT NULL,
+          email TEXT NOT NULL,
+          raw_json JSONB,
+          created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL
           );
-        SQL
+          SQL
 
         Lustra::SQL.execute "COMMENT ON COLUMN admins.email IS 'Primary email address';"
         Lustra::SQL.execute "CREATE UNIQUE INDEX admins_provider_uid ON admins (provider, uid);"
@@ -30,12 +30,12 @@ module IntrospectionSpec
         info.schema.should eq("public")
         info.table.should eq("admins")
 
-        email = info.columns.find(&.name.==("email")).not_nil!
+        email = info.columns.find!(&.name.==("email"))
         email.data_type.should eq("text")
         email.nullable.should be_false
         email.description.should eq("Primary email address")
 
-        id_col = info.columns.find(&.name.==("id")).not_nil!
+        id_col = info.columns.find!(&.name.==("id"))
         id_col.default_value.should_not be_nil
         id_col.default_value.not_nil!.should contain("nextval")
 
@@ -44,10 +44,10 @@ module IntrospectionSpec
         idx_names.should contain("admins_provider_uid")
         idx_names.should contain("admins_created_at")
 
-        pk = info.indexes.find(&.primary).not_nil!
+        pk = info.indexes.find!(&.primary)
         pk.unique.should be_true
 
-        provider_uid = info.indexes.find(&.name.==("admins_provider_uid")).not_nil!
+        provider_uid = info.indexes.find!(&.name.==("admins_provider_uid"))
         provider_uid.unique.should be_true
         provider_uid.primary.should be_false
       end
