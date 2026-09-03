e93850d6278dde03fbea729b50c5032c73c98031
Anton Maminov <anton.linux@gmail.com>
2026-09-03 12:36:33 +0300

    Add raw SQL streaming model benchmark

--- sample/benchmark/model.cr
+++ sample/benchmark/model.cr
@@ -28,13 +28,45 @@ class BenchmarkModel
   column y : Int32
 end
 
+LIMIT = 100_000
+
 puts "Starting benchmarking, total to fetch = #{BenchmarkModel.query.count} records"
 
 Benchmark.ips(warmup: 2.seconds, calculation: 5.seconds) do |x|
-  x.report("With Model: Simple load 100k") { BenchmarkModel.query.limit(100_000).to_a }
-  x.report("With Model: With cursor") { a = [] of BenchmarkModel; BenchmarkModel.query.limit(100_000).each_with_cursor { |o| a << o } }
-  x.report("With Model: With attributes") { BenchmarkModel.query.limit(100_000).to_a(fetch_columns: true) }
-  x.report("With Model: With attributes and cursor") { a = [] of BenchmarkModel; BenchmarkModel.query.limit(100_000).each_with_cursor(fetch_columns: true) { |h| a << h } }
-  x.report("Using: Pluck") { BenchmarkModel.query.limit(100_000).pluck("y") }
-  x.report("Hash from SQL only") { a = [] of Hash(String, ::Lustra::SQL::Any); BenchmarkModel.query.limit(100_000).fetch { |h| a << h.dup } }
+  x.report("With Model: Simple load 100k") do
+    BenchmarkModel.query.limit(LIMIT).to_a
+  end
+
+  x.report("With Model: With cursor") do
+    a = [] of BenchmarkModel
+    BenchmarkModel.query.limit(LIMIT).each_with_cursor { |o| a << o }
+  end
+
+  x.report("With Model: With attributes") do
+    BenchmarkModel.query.limit(LIMIT).to_a(fetch_columns: true)
+  end
+
+  x.report("With Model: With attributes and cursor") do
+    a = [] of BenchmarkModel
+    BenchmarkModel.query.limit(LIMIT).each_with_cursor(fetch_columns: true) { |h| a << h }
+  end
+
+  x.report("Hash from SQL only") do
+    a = [] of Hash(String, ::Lustra::SQL::Any)
+    BenchmarkModel.query.limit(LIMIT).fetch { |h| a << h.dup }
+  end
+
+  x.report("Raw SQL typed streaming") do
+    results = [] of Hash(String, ::Lustra::SQL::Any)
+
+    Lustra::SQL::ConnectionPool.with_connection("default") do |cnx|
+      cnx.query("SELECT id, y FROM benchmark LIMIT #{LIMIT}") do |rs|
+        rs.each do
+          results << {"id" => rs.read(Int32), "y" => rs.read(Int32)} of String => Lustra::SQL::Any
+        end
+      end
+    end
+
+    results
+  end
 end
