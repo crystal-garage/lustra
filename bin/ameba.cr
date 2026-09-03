491fa7e85e3bb3f9b4d4c236cd23c4a64c1b68fa
Anton Maminov <anton.linux@gmail.com>
2026-09-03 11:20:28 +0300

    Delete ameba.cr

--- bin/ameba.cr
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env crystal
-
-# Require ameba extensions here which are added as project dependencies.
-# Example:
-#
-# require "ameba-performance"
-
-# Require ameba cli which starts the inspection.
-require "ameba/cli"
